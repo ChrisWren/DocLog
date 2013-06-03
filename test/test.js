045a1a4214cd13916e61865af4498d809b167271
ChrisWren <cthewren@gmail.com>
2013-06-02 21:58:51 -0700

    Added smoke test

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+require('should');
+var spawn = require('child_process').spawn;
+
+describe('When the theme is built', function () {
+
+  before(function (done) {
+    var buildProcess = spawn('grunt', ['build']);
+    buildProcess.stdout.on('close', function () {
+      done();
+    });
+  });
+
+  it('it creates posts in the expected location', function() {
+    fs.existsSync('.tmp/index.html').should.be.ok;
+    fs.existsSync('.tmp/Team.html').should.be.ok;
+  });
+});
